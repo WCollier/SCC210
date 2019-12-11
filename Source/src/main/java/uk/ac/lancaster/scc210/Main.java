4ca6c0cc988d8a6e9630e96e48093cd18ee1d2ca
WCollier <w.collier@lancaster.ac.uk>
2019-12-11 19:04:45 +0000

    Add starting point.

--- Source/src/main/java/uk/ac/lancaster/scc210/Main.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/Main.java
@@ -1,10 +1,33 @@
 package uk.ac.lancaster.scc210;
 
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
+
 public class Main {
-    int add(int left, int right) {
-        return left + right;
+    private final int WIDTH = 500;
+
+    private final int HEIGHT = 500;
+
+    private RenderWindow window;
+
+    private Main() {
+        window = new RenderWindow(new VideoMode(WIDTH, HEIGHT), "Game");
+
+        window.setFramerateLimit(60);
+
+        Event event;
+
+        while (window.isOpen()) {
+            while ((event = window.pollEvent()) != null) {
+                if (event.type == Event.Type.CLOSED) {
+                    window.close();
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
+        new Main();
     }
 }
--- Source/src/test/java/uk/ac/lancaster/scc210/MainTest.java
+++ Source/src/test/java/uk/ac/lancaster/scc210/MainTest.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    private final Main main = new Main();
-
     @Test
     void add() {
-        assertEquals(2, main.add(1, 1));
     }
 }
