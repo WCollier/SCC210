c2a26c160a7e585699c65f45c2ac12083beb60be
WCollier <w.collier@lancaster.ac.uk>
2020-01-15 13:54:29 +0000

    Remove textureAtlases

--- Source/src/main/java/uk/ac/lancaster/scc210/content/TextureManager.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/content/TextureManager.java
@@ -3,7 +3,6 @@ package uk.ac.lancaster.scc210.content;
 import org.jsfml.graphics.Texture;
 import uk.ac.lancaster.scc210.resources.deserialise.SerialisedTextureAtlas;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TextureManager extends ContentManager<Texture> {
@@ -11,21 +10,15 @@ public class TextureManager extends ContentManager<Texture> {
 
     private static final int SPRITE_HEIGHT = 64;
 
-    private List<TextureAtlas> textureAtlases;
-
     public TextureManager(List<SerialisedTextureAtlas> serialisedTextureAtlases) {
         super(new MissingTexture(SPRITE_WIDTH, SPRITE_HEIGHT).getTexture());
-
-        textureAtlases = new ArrayList<>();
 
         serialisedTextureAtlases.forEach(serialisedTextureAtlas -> {
             TextureAtlas textureAtlas = serialisedTextureAtlas.getTextureAtlas();
 
-            serialisedTextureAtlas.getSerialisedTextures().forEach(texture ->
-                    put(texture.getName(), texture.getTexture())
-            );
-
-            textureAtlases.add(textureAtlas);
+            serialisedTextureAtlas.getSerialisedTextures()
+                    .parallelStream()
+                    .forEach(texture -> put(texture.getName(), texture.getTexture()));
         });
     }
 }
