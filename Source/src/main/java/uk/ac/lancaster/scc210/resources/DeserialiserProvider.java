c942f315e37a8638260cd52b77178902b2dd2d7f
WCollier <w.collier@lancaster.ac.uk>
2020-01-15 13:52:36 +0000

    Rename DeserialiserProvider

--- Source/src/main/java/uk/ac/lancaster/scc210/resources/DeserialiserProvider.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/resources/DeserialiserProvider.java
@@ -1,40 +1,30 @@
 package uk.ac.lancaster.scc210.resources;
 
-import uk.ac.lancaster.scc210.content.SpaceShipManager;
-import uk.ac.lancaster.scc210.content.TextureManager;
 import uk.ac.lancaster.scc210.resources.deserialise.SpaceShipDeserialiser;
 import uk.ac.lancaster.scc210.resources.deserialise.TextureAtlasDeserialiser;
 
-public class ResourceProvider {
-    private TextureManager textureManager;
-
-    private SpaceShipManager spaceShipManager;
-
+public class DeserialiserProvider {
     private TextureAtlasDeserialiser textureAtlasDeserialiser;
 
     private SpaceShipDeserialiser spaceShipDeserialiser;
 
-    public ResourceProvider() throws ResourceNotFoundException {
+    public DeserialiserProvider() throws ResourceNotFoundException {
         XMLAdapter xmlAdapter = new XMLAdapter();
 
         ResourceLoader.loadFromFile(xmlAdapter, "textures.xml");
 
         textureAtlasDeserialiser = new TextureAtlasDeserialiser(xmlAdapter.getResource());
 
-        textureManager = new TextureManager(textureAtlasDeserialiser.getSerialisedTextureAtlases());
-
         ResourceLoader.loadFromFile(xmlAdapter, "spaceships.xml");
 
         spaceShipDeserialiser = new SpaceShipDeserialiser(xmlAdapter.getResource());
-
-        spaceShipManager = new SpaceShipManager(textureManager, spaceShipDeserialiser.getSerialisedSpaceShips());
     }
 
-    public TextureManager getTextureManager() {
-        return textureManager;
+    public TextureAtlasDeserialiser getTextureAtlasDeserialiser() {
+        return textureAtlasDeserialiser;
     }
 
-    public SpaceShipManager getSpaceShipManager() {
-        return spaceShipManager;
+    public SpaceShipDeserialiser getSpaceShipDeserialiser() {
+        return spaceShipDeserialiser;
     }
 }
