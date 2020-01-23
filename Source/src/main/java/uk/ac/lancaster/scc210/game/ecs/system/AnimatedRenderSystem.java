6a28e75d887cfac0fada6edea1557253c46d8646
WCollier <w.collier@lancaster.ac.uk>
2020-01-23 14:14:48 +0000

    Add Animated Render System

---
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/system/AnimatedRenderSystem.java
@@ -0,0 +1,40 @@
+package uk.ac.lancaster.scc210.game.ecs.system;
+
+import org.jsfml.graphics.RenderTarget;
+import uk.ac.lancaster.scc210.engine.ecs.Entity;
+import uk.ac.lancaster.scc210.engine.ecs.World;
+import uk.ac.lancaster.scc210.engine.ecs.system.IterativeSystem;
+import uk.ac.lancaster.scc210.game.ecs.component.AnimationComponent;
+import uk.ac.lancaster.scc210.game.ecs.component.SpriteComponent;
+
+/**
+ * The type Render system.
+ */
+public class AnimatedRenderSystem extends IterativeSystem {
+    /**
+     * Instantiates a new Render system.
+     *
+     * @param world the world
+     */
+    public AnimatedRenderSystem(World world) {
+        super(world, SpriteComponent.class, AnimationComponent.class);
+    }
+
+    @Override
+    public void draw(RenderTarget target) {
+        for (Entity entity : entities) {
+            SpriteComponent spriteComponent = (SpriteComponent) entity.findComponent(SpriteComponent.class);
+
+            AnimationComponent animationComponent = (AnimationComponent) entity.findComponent(AnimationComponent.class);
+
+            spriteComponent.getSprite().setTexture(animationComponent.getTextureAnimation().getTexture());
+
+            target.draw(spriteComponent.getSprite());
+        }
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
