d3263724e7258dfc56539ab4907b14576371f451
buzatu <a.buzatu@lancaster.ac.uk>
2020-01-31 21:55:20 +0000

    Added sprites in all directions (general)

--- Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/system/FiringSystem.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/system/FiringSystem.java
@@ -50,7 +50,9 @@ public class FiringSystem extends IterativeSystem {
                 // Find the half-width of the entity sprite and the half-width of the bullet sprite
                 float startPoint =  - bulletSprite[1].getSprite().getLocalBounds().width/2;
                 float width = entitySprite.getSprite().getLocalBounds().width;
-                float difference = (float) (width*(Math.sqrt(2) - 1)/2);
+                float height = entitySprite.getSprite().getLocalBounds().height;
+                float equalDistance = (float) ( Math.sqrt(Math.pow(height,2) + Math.pow(width,2))/Math.sqrt(2));
+                float difference = (float) (equalDistance*(Math.sqrt(2) - 1)/2);
 
 
 
@@ -58,13 +60,13 @@ public class FiringSystem extends IterativeSystem {
                 Vector2f[] coordonates = new Vector2f[8];
 
                  coordonates[0] = new Vector2f(startPoint , startPoint);
-                 coordonates[1] = new Vector2f( startPoint +width/2 ,  - difference);
-                 coordonates[2] = new Vector2f(startPoint/2 + width , startPoint);
-                 coordonates[3] = new Vector2f(startPoint + width + difference  , startPoint + width/2);
-                 coordonates[4] = new Vector2f( width,  width);
-                 coordonates[5] = new Vector2f(-startPoint + width/2 , width + difference );
+                 coordonates[1] = new Vector2f( startPoint +equalDistance/2 ,  - difference);
+                 coordonates[2] = new Vector2f(startPoint/2 + equalDistance , startPoint);
+                 coordonates[3] = new Vector2f(startPoint + equalDistance + difference  , startPoint + equalDistance/2);
+                 coordonates[4] = new Vector2f( equalDistance,  equalDistance);
+                 coordonates[5] = new Vector2f(-startPoint + equalDistance/2 , equalDistance + difference );
                  coordonates[6] = new Vector2f( -startPoint, width - startPoint);
-                 coordonates[7] = new Vector2f(-startPoint - difference , -startPoint + width/2);
+                 coordonates[7] = new Vector2f(-startPoint - difference , -startPoint + equalDistance/2);
 
 
 
