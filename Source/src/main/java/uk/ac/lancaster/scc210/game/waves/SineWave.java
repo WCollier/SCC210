dd0b0378b2cc4eadea592897a0a1c147dc4c724a
WCollier <w.collier@lancaster.ac.uk>
2020-02-06 23:18:42 +0000

    Adapt waves to use Transformable.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/waves/SineWave.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/waves/SineWave.java
@@ -1,11 +1,11 @@
 package uk.ac.lancaster.scc210.game.waves;
 
-import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.Transformable;
 import org.jsfml.system.Time;
 import org.jsfml.system.Vector2f;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.game.ecs.component.SpeedComponent;
-import uk.ac.lancaster.scc210.game.ecs.component.SpriteComponent;
+import uk.ac.lancaster.scc210.game.ecs.component.TransformableComponent;
 
 import java.util.Set;
 
@@ -29,15 +29,15 @@ public class SineWave extends Wave {
         double waveAngle = elapsedTime.asSeconds() * Math.PI;
 
         for (Entity entity : entities) {
-            SpriteComponent spriteComponent = (SpriteComponent) entity.findComponent(SpriteComponent.class);
+            TransformableComponent transformableComponent = (TransformableComponent) entity.findComponent(TransformableComponent.class);
+
+            Transformable transformable = transformableComponent.getTransformable();
 
             SpeedComponent speedComponent = (SpeedComponent) entity.findComponent(SpeedComponent.class);
 
-            Sprite sprite = spriteComponent.getSprite();
-
             float speed = speedComponent.getSpeed();
 
-            calculateMoveToPoint(sprite.getPosition());
+            calculateMoveToPoint(transformable.getPosition());
 
             Vector2f perpendicular = new Vector2f(-direction.y, direction.x);
 
@@ -45,14 +45,14 @@ public class SineWave extends Wave {
 
             Vector2f wave = new Vector2f((float) (perpendicular.x * waveCurve), (float) (perpendicular.y * waveCurve));
 
-            sprite.setRotation(rotateSprite());
+            transformable.setRotation(rotateSprite());
 
             // If the entity goes out of bounds, reset the entity back to it's starting position
-            if (passedDestination(sprite.getPosition())) {
-                sprite.setPosition(origin);
+            if (passedDestination(transformable.getPosition())) {
+                transformable.setPosition(origin);
 
             } else {
-                sprite.move((direction.x * speed) + wave.x, (direction.y * speed) + wave.y);
+                transformable.move((direction.x * speed) + wave.x, (direction.y * speed) + wave.y);
             }
         }
     }
