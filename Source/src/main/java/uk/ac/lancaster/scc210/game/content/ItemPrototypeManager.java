b63bc54881b0f5398965e2cc22fa4aaeff6478a7
WCollier <w.collier@lancaster.ac.uk>
2020-02-07 14:48:32 +0000

    ItemPrototype now has ItemEffects. Expand example item in ItemManager

--- Source/src/main/java/uk/ac/lancaster/scc210/game/content/ItemPrototypeManager.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/content/ItemPrototypeManager.java
@@ -2,13 +2,13 @@ package uk.ac.lancaster.scc210.game.content;
 
 import uk.ac.lancaster.scc210.engine.content.ContentManager;
 import uk.ac.lancaster.scc210.engine.content.TextureManager;
+import uk.ac.lancaster.scc210.game.items.SpeedIncreaseEffect;
 import uk.ac.lancaster.scc210.game.prototypes.ItemPrototype;
 
 public class ItemPrototypeManager extends ContentManager<ItemPrototype> {
     public ItemPrototypeManager(TextureManager textureManager) {
         super(null);
 
-        put("test-item", new ItemPrototype(textureManager.get("spritesheet.png:example")));
-        put("test-item", new ItemPrototype(textureManager.get("spritesheet.png:other")));
+        put("test-item", new ItemPrototype(textureManager.get("spritesheet.png:example"), new SpeedIncreaseEffect()));
     }
 }
--- Source/src/main/java/uk/ac/lancaster/scc210/game/prototypes/ItemPrototype.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/prototypes/ItemPrototype.java
@@ -5,18 +5,29 @@ import org.jsfml.graphics.Texture;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.engine.ecs.World;
 import uk.ac.lancaster.scc210.engine.prototypes.Prototype;
+import uk.ac.lancaster.scc210.game.ecs.component.ItemEffectsComponent;
 import uk.ac.lancaster.scc210.game.ecs.component.SpriteComponent;
+import uk.ac.lancaster.scc210.game.items.ItemEffect;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ItemPrototype implements Prototype {
     private final Texture texture;
 
-    public ItemPrototype(Texture texture) {
+    private final Set<ItemEffect> itemEffects;
+
+    public ItemPrototype(Texture texture, ItemEffect... itemEffects) {
         this.texture = texture;
+        this.itemEffects = new HashSet<>(Arrays.asList(itemEffects));
     }
 
     public Entity create() {
-        final SpriteComponent spriteComponent = new SpriteComponent(new Sprite(texture)); //TODO: what goes here?
+        final SpriteComponent spriteComponent = new SpriteComponent(new Sprite(texture));
 
-        return World.createEntity(spriteComponent);
+        final ItemEffectsComponent itemEffectsComponent = new ItemEffectsComponent(itemEffects);
+
+        return World.createEntity(spriteComponent, itemEffectsComponent);
     }
 }
