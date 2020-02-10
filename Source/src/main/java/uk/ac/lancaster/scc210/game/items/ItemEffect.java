c0987f37a0e99e2edff3939cc2ee6d2f3283ce36
WCollier <w.collier@lancaster.ac.uk>
2020-02-10 19:58:57 +0000

    ItemEffects now have the ability to reset to the default state.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/items/ItemEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/ItemEffect.java
@@ -2,6 +2,22 @@ package uk.ac.lancaster.scc210.game.items;
 
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 
+/**
+ * This interfaces describes how an item should react after it has been collided with.
+ */
 public interface ItemEffect {
+    /**
+     * Describes what should happen to the entity when it collides with an item
+     *
+     * @param entity the entity to react to
+     */
     void react(Entity entity);
+
+    /**
+     * At certain stages (e.g. at the end of a level) the item effects are reset to the default. This should be used
+     * to reset.
+     *
+     * @param entity the entity to reset
+     */
+    void reset(Entity entity);
 }
--- Source/src/main/java/uk/ac/lancaster/scc210/game/items/SpeedIncreaseEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/SpeedIncreaseEffect.java
@@ -4,12 +4,27 @@ import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.game.ecs.component.SpeedComponent;
 
 public class SpeedIncreaseEffect implements ItemEffect {
+    private int oldSpeed;
+
     @Override
     public void react(Entity entity) {
         if (entity.hasComponent(SpeedComponent.class)) {
             SpeedComponent speedComponent = (SpeedComponent) entity.findComponent(SpeedComponent.class);
 
+            oldSpeed = speedComponent.getSpeed();
+
             speedComponent.setSpeed(20);
+        }
+    }
+
+    @Override
+    public void reset(Entity entity) {
+        if (entity.hasComponent(SpeedComponent.class)) {
+            SpeedComponent speedComponent = (SpeedComponent) entity.findComponent(SpeedComponent.class);
+
+            speedComponent.setSpeed(oldSpeed);
+
+            System.out.println("Resetting speed");
         }
     }
 }
