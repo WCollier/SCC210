cda60850554ce1d66022339b996bc6c711627afc
WCollier <w.collier@lancaster.ac.uk>
2020-02-10 16:25:15 +0000

    Prevent being destroyed by own bullets.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/system/BulletCollisionSystem.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/system/BulletCollisionSystem.java
@@ -10,6 +10,7 @@ import uk.ac.lancaster.scc210.engine.ecs.system.IterativeSystem;
 import uk.ac.lancaster.scc210.game.ecs.component.BulletComponent;
 import uk.ac.lancaster.scc210.game.ecs.component.OrientatedBoxComponent;
 import uk.ac.lancaster.scc210.game.ecs.component.SpaceShipComponent;
+import uk.ac.lancaster.scc210.game.ecs.component.WaveComponent;
 
 import java.util.Set;
 
@@ -39,12 +40,32 @@ public class BulletCollisionSystem extends IterativeSystem {
         for (Entity entity : entities) {
             OrientatedBoxComponent entityOrientedBox = (OrientatedBoxComponent) entity.findComponent(OrientatedBoxComponent.class);
 
+            BulletComponent bulletComponent = (BulletComponent) entity.findComponent(BulletComponent.class);
+
+            Entity bulletCreator = bulletComponent.getCreator();
+
             for (Entity spaceShip : spaceShips) {
                 OrientatedBoxComponent spaceShipOrientedBox = (OrientatedBoxComponent) spaceShip.findComponent(OrientatedBoxComponent.class);
 
                 boolean colliding = OrientatedBox.areColliding(entityOrientedBox.getOrientatedBox(), spaceShipOrientedBox.getOrientatedBox());
 
-                if (colliding) {
+                boolean sameWave = false;
+
+                /*
+                TODO: Change this to use EnemyComponent instead
+                If the creator of the bullet has a wave and the spaceShip belongs to a wave, find if the creator
+                of the bullet and the current spaceship belong to the same wave. If so, prevent colliding with spaceships
+                of the same wave.
+                 */
+                if (bulletComponent.getCreator().hasComponent(WaveComponent.class) && spaceShip.hasComponent(WaveComponent.class)) {
+                    WaveComponent bulletWaveComponent = (WaveComponent) entity.findComponent(WaveComponent.class);
+
+                    WaveComponent spaceShipWaveComponent = (WaveComponent) spaceShip.findComponent(WaveComponent.class);
+
+                    sameWave = bulletWaveComponent.getWave() == spaceShipWaveComponent.getWave();
+                }
+
+                if (colliding && bulletCreator != spaceShip && !sameWave) {
                     PooledComponent pooledComponent = (PooledComponent) entity.findComponent(PooledComponent.class);
 
                     world.removeEntity(spaceShip);
