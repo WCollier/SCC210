0b7acc515218417e7a8bddce3a1764a13008ffbb
WCollier <w.collier@lancaster.ac.uk>
2020-02-12 13:55:57 +0000

    When the player collides with a SpaceShip, the player is now removed from the world.

---
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/system/SpaceShipCollisionSystem.java
@@ -0,0 +1,66 @@
+package uk.ac.lancaster.scc210.game.ecs.system;
+
+import org.jsfml.graphics.RenderTarget;
+import org.jsfml.system.Time;
+import uk.ac.lancaster.scc210.engine.collision.OrientatedBox;
+import uk.ac.lancaster.scc210.engine.ecs.Entity;
+import uk.ac.lancaster.scc210.engine.ecs.World;
+import uk.ac.lancaster.scc210.engine.ecs.system.IterativeSystem;
+import uk.ac.lancaster.scc210.game.ecs.component.OrientatedBoxComponent;
+import uk.ac.lancaster.scc210.game.ecs.component.PlayerComponent;
+import uk.ac.lancaster.scc210.game.ecs.component.SpaceShipComponent;
+
+import java.util.Optional;
+
+public class SpaceShipCollisionSystem extends IterativeSystem {
+    private Optional<Entity> player;
+
+    /**
+     * Instantiates a new Iterative system.
+     *
+     * @param world the world containing entities to use
+     */
+    public SpaceShipCollisionSystem(World world) {
+        // TODO: Use EnemyComponent here
+        super(world, SpaceShipComponent.class);
+
+        player = world.getEntitiesFor(PlayerComponent.class).stream().findFirst();
+    }
+
+    @Override
+    public void entityAdded(Entity entity) {
+        super.entityAdded(entity);
+
+        player = world.getEntitiesFor(PlayerComponent.class).stream().findFirst();
+    }
+
+    @Override
+    public void update(Time deltaTime) {
+        if (player.isEmpty()) {
+            return;
+        }
+
+        Entity playerEntity = player.get();
+
+        for (Entity entity : entities) {
+            // Player can't collide with themselves
+            if (playerEntity == entity) {
+                continue;
+            }
+
+            OrientatedBoxComponent playerOrientedBox = (OrientatedBoxComponent) playerEntity.findComponent(OrientatedBoxComponent.class);
+
+            OrientatedBoxComponent entityOrientedBox = (OrientatedBoxComponent) entity.findComponent(OrientatedBoxComponent.class);
+
+            if (OrientatedBox.areColliding(playerOrientedBox.getOrientatedBox(), entityOrientedBox.getOrientatedBox())) {
+                System.out.println("Called");
+
+                world.removeEntity(playerEntity);
+            }
+        }
+    }
+
+    @Override
+    public void draw(RenderTarget target) {
+    }
+}
