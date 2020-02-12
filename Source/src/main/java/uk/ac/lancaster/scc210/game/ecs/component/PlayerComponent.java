626e1feeaf3816bf13dc3dbcfd45714ab8247275
WCollier <w.collier@lancaster.ac.uk>
2020-02-12 13:56:17 +0000

    Prevent the currentEffects being null when constructed.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/component/PlayerComponent.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/component/PlayerComponent.java
@@ -3,10 +3,15 @@ package uk.ac.lancaster.scc210.game.ecs.component;
 import uk.ac.lancaster.scc210.engine.ecs.Component;
 import uk.ac.lancaster.scc210.game.items.ItemEffect;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class PlayerComponent implements Component {
     private Set<ItemEffect> currentEffects;
+
+    public PlayerComponent() {
+        currentEffects = new HashSet<>();
+    }
 
     public Set<ItemEffect> getCurrentEffects() {
         return currentEffects;
