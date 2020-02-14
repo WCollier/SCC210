4b09750d9146c604e4cd9f7fb10afabcb9aaa633
WCollier <w.collier@lancaster.ac.uk>
2020-02-14 14:51:39 +0000

    Prevent stationary entities from being continually re-spawned.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/level/LevelStage.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/level/LevelStage.java
@@ -1,6 +1,8 @@
 package uk.ac.lancaster.scc210.game.level;
 
+import org.jsfml.system.Time;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
+import uk.ac.lancaster.scc210.engine.ecs.World;
 
 import java.util.List;
 
@@ -9,15 +11,39 @@ public class LevelStage {
 
     private final List<Entity> stationaryEntities;
 
+    private boolean stationarySpawned;
+
     public LevelStage(List<LevelWave> waves, List<Entity> stationaryEntities) {
         this.waves = waves;
         this.stationaryEntities = stationaryEntities;
+
+        stationarySpawned = false;
     }
 
     public boolean complete() {
         // All are complete
         return waves.parallelStream().allMatch(LevelWave::complete) && stationaryEntities.isEmpty();
     }
+
+    public void spawn(World world, Time deltaTime) {
+        // Prevent stationary entities from being re-spawned
+        if (!stationarySpawned) {
+            world.addEntities(stationaryEntities);
+
+            stationarySpawned = true;
+        }
+
+        for (LevelWave wave : waves) {
+            Entity newShip = wave.spawnNew(deltaTime);
+
+            if (newShip != null) {
+                world.addEntity(newShip);
+            }
+
+            wave.getWave().update(wave.getEntities(), deltaTime);
+        }
+    }
+
 
     public List<LevelWave> getWaves() {
         return waves;
