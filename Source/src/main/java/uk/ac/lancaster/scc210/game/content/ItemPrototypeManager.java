83b6d895e150d23436cf75c5e647d66ccaabce67
atkinsx <x.atkins@lancaster.ac.uk>
2020-02-20 22:09:48 +0000

    Added an Item that increases the Player's lives. Now attempting to add an item that changes bullet patterns.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/content/ItemPrototypeManager.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/content/ItemPrototypeManager.java
@@ -2,6 +2,7 @@ package uk.ac.lancaster.scc210.game.content;
 
 import uk.ac.lancaster.scc210.engine.content.ContentManager;
 import uk.ac.lancaster.scc210.engine.content.TextureManager;
+import uk.ac.lancaster.scc210.game.items.LivesIncreaseEffect;
 import uk.ac.lancaster.scc210.game.items.SpeedIncreaseEffect;
 import uk.ac.lancaster.scc210.game.prototypes.ItemPrototype;
 
@@ -9,8 +10,8 @@ public class ItemPrototypeManager extends ContentManager<ItemPrototype> {
     public ItemPrototypeManager(TextureManager textureManager) {
         super(null);
 
-        put("test-item", new ItemPrototype(textureManager.get("spritesheet.png:example"), new SpeedIncreaseEffect()));
+        put("test-item", new ItemPrototype(textureManager.get("spritesheet.png:item"), new SpeedIncreaseEffect()));
 
-        put("other-item", new ItemPrototype(textureManager.get("spritesheet.png:item")));
+        put("other-item", new ItemPrototype(textureManager.get("spritesheet.png:example"), new LivesIncreaseEffect()));
     }
 }
