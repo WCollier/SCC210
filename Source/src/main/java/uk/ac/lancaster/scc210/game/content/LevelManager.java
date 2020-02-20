80768ce06cde354a8b5fa0e7a4efe9b2c4772bc1
WCollier <w.collier@lancaster.ac.uk>
2020-02-20 20:54:58 +0000

    LevelManager now has a Queue<Level> for storing the levels as they are inserted.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/content/LevelManager.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/content/LevelManager.java
@@ -4,19 +4,28 @@ import uk.ac.lancaster.scc210.engine.content.ContentManager;
 import uk.ac.lancaster.scc210.game.level.Level;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LevelManager extends ContentManager<Level> {
+    // As a HashMap can't ensure ordering, use a queue to enter levels as they are inserted, to ensure ordering
+    private Queue<Level> levelQueue;
+
     public LevelManager(List<Level> levels) {
         super(null);
 
-        int i = 0;
+        levelQueue = new LinkedList<>();
+
+        levelQueue.addAll(levels);
 
         for (Level level : levels) {
-            put(String.valueOf(i), level);
-
-            i++;
+            put(level.getName(), level);
         }
+    }
+
+    public Queue<Level> getLevelQueue() {
+        return levelQueue;
     }
 
     public Iterator<Level> getIterator() {
