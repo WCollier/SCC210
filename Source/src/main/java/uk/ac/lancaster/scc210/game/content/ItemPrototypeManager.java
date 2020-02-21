979fa5712e060dbf35b735c37543dd7e7400fb07
buzatu <a.buzatu@lancaster.ac.uk>
2020-02-21 23:33:10 +0000

    Add Morning Star Pattern Effect

--- Source/src/main/java/uk/ac/lancaster/scc210/game/content/ItemPrototypeManager.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/content/ItemPrototypeManager.java
@@ -2,10 +2,7 @@ package uk.ac.lancaster.scc210.game.content;
 
 import uk.ac.lancaster.scc210.engine.content.ContentManager;
 import uk.ac.lancaster.scc210.engine.content.TextureManager;
-import uk.ac.lancaster.scc210.game.items.LinePatternEffect;
-import uk.ac.lancaster.scc210.game.items.LivesIncreaseEffect;
-import uk.ac.lancaster.scc210.game.items.SpeedIncreaseEffect;
-import uk.ac.lancaster.scc210.game.items.StarPatternEffect;
+import uk.ac.lancaster.scc210.game.items.*;
 import uk.ac.lancaster.scc210.game.pooling.BulletPool;
 import uk.ac.lancaster.scc210.game.prototypes.ItemPrototype;
 
@@ -20,5 +17,7 @@ public class ItemPrototypeManager extends ContentManager<ItemPrototype> {
         put("starPattern-item", new ItemPrototype(textureManager.get("spritesheet.png:example"), new StarPatternEffect(bulletPool)));
 
         put("linePattern-item", new ItemPrototype(textureManager.get("spritesheet.png:example"), new LinePatternEffect(bulletPool)));
+
+        put("morningStarPattern-item", new ItemPrototype(textureManager.get("spritesheet.png:example"), new MorningStarPatternEffect(bulletPool)));
     }
 }
