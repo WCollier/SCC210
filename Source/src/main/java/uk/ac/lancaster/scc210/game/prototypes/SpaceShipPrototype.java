ae487067343599901bbc1df64e53abdfa8adb271
buzatu <a.buzatu@lancaster.ac.uk>
2020-02-21 02:13:16 +0000

    Bullet Cross Pattern

--- Source/src/main/java/uk/ac/lancaster/scc210/game/prototypes/SpaceShipPrototype.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/prototypes/SpaceShipPrototype.java
@@ -9,7 +9,7 @@ import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.engine.ecs.World;
 import uk.ac.lancaster.scc210.engine.pooling.Pool;
 import uk.ac.lancaster.scc210.engine.prototypes.Prototype;
-import uk.ac.lancaster.scc210.game.bullets.patterns.StraightLinePattern;
+import uk.ac.lancaster.scc210.game.bullets.patterns.CrossPattern;
 import uk.ac.lancaster.scc210.game.ecs.component.*;
 import uk.ac.lancaster.scc210.game.resources.SerialisedSpaceShip;
 
@@ -71,7 +71,7 @@ public class SpaceShipPrototype implements Prototype {
         Entity spaceShip = World.createEntity(animationComponent, spriteComponent, speedComponent, rotationComponent, spaceShipComponent, orientatedBoxComponent, transformableComponent, scoreComponent, livesComponent, flashComponent);
 
         // Let's assume ships will use the straight line pattern for now
-        final FiringPatternComponent firingPatternComponent = new FiringPatternComponent(new StraightLinePattern(pool, spaceShip, bulletName));
+        final FiringPatternComponent firingPatternComponent = new FiringPatternComponent(new CrossPattern(pool, spaceShip, bulletName));
 
         spaceShip.addComponent(firingPatternComponent);
 
