f19c00706d3d33800ce169313d4c1a45c0d1351d
WCollier <w.collier@lancaster.ac.uk>
2020-02-21 23:12:01 +0000

    Abstract the firing pattern items into a superclass.

---
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/FiringPatternEffect.java
@@ -0,0 +1,58 @@
+package uk.ac.lancaster.scc210.game.items;
+
+import uk.ac.lancaster.scc210.engine.ecs.Entity;
+import uk.ac.lancaster.scc210.engine.pooling.Pool;
+import uk.ac.lancaster.scc210.game.bullets.patterns.Pattern;
+import uk.ac.lancaster.scc210.game.ecs.component.FiringPatternComponent;
+import uk.ac.lancaster.scc210.game.ecs.component.SpaceShipComponent;
+
+public abstract class FiringPatternEffect implements ItemEffect {
+    protected Pool pool;
+
+    protected Pattern pattern;
+
+    private Pattern oldPattern;
+
+    FiringPatternEffect(Pool pool) {
+        this.pool = pool;
+    }
+
+    /**
+     * Describes what should happen to the entity when it collides with an item
+     *
+     * @param entity the entity to react to
+     */
+    @Override
+    public void react(Entity entity) {
+        if (entity.hasComponent(FiringPatternComponent.class) && entity.hasComponent(SpaceShipComponent.class)) {
+            FiringPatternComponent firingPatternComponent = (FiringPatternComponent) entity.findComponent(FiringPatternComponent.class);
+
+            if (oldPattern == null) {
+                oldPattern = firingPatternComponent.getPattern();
+            }
+
+            if (pattern == null) {
+                setPattern(entity);
+            }
+
+            firingPatternComponent.setPattern(pattern);
+        }
+    }
+
+    /**
+     * At certain stages (e.g. at the end of a level) the item effects are reset to the default. This should be used
+     * to reset.
+     *
+     * @param entity the entity to reset
+     */
+    @Override
+    public void reset(Entity entity) {
+        if (entity.hasComponent(FiringPatternComponent.class) && entity.hasComponent(SpaceShipComponent.class)) {
+            FiringPatternComponent firingPatternComponent = (FiringPatternComponent) entity.findComponent(FiringPatternComponent.class);
+
+            firingPatternComponent.setPattern(oldPattern);
+        }
+    }
+
+    abstract void setPattern(Entity entity);
+}
--- Source/src/main/java/uk/ac/lancaster/scc210/game/items/StarPatternEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/StarPatternEffect.java
@@ -2,54 +2,18 @@ package uk.ac.lancaster.scc210.game.items;
 
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.engine.pooling.Pool;
-import uk.ac.lancaster.scc210.game.bullets.patterns.Pattern;
 import uk.ac.lancaster.scc210.game.bullets.patterns.StarPattern;
-import uk.ac.lancaster.scc210.game.ecs.component.FiringPatternComponent;
 import uk.ac.lancaster.scc210.game.ecs.component.SpaceShipComponent;
 
-public class StarPatternEffect implements ItemEffect {
-    private Pattern oldPattern;
-
-    private Pool pool;
-
-    public StarPatternEffect (Pool pool)
-    {
-        this.pool = pool;
+public class StarPatternEffect extends FiringPatternEffect {
+    public StarPatternEffect(Pool pool) {
+        super(pool);
     }
 
     @Override
-    public void react(Entity entity) {
+    void setPattern(Entity entity) {
+        SpaceShipComponent spaceShipComponent = (SpaceShipComponent) entity.findComponent(SpaceShipComponent.class);
 
-
-        if (entity.hasComponent(FiringPatternComponent.class ) && entity.hasComponent(SpaceShipComponent.class)){
-
-
-            SpaceShipComponent spaceShipComponent = (SpaceShipComponent) entity.findComponent(SpaceShipComponent.class);
-
-            FiringPatternComponent firingPatternComponent = (FiringPatternComponent) entity.findComponent(FiringPatternComponent.class);
-
-            if (oldPattern == null) {
-                oldPattern = firingPatternComponent.getPattern();
-            }
-
-            firingPatternComponent.setPattern(new StarPattern(pool, entity, spaceShipComponent.getBulletName()));
-
-        }
-
-
-    }
-
-    @Override
-    public void reset(Entity entity) {
-        if (entity.hasComponent(FiringPatternComponent.class) && entity.hasComponent(SpaceShipComponent.class)) {
-
-
-            FiringPatternComponent firingPatternComponent = (FiringPatternComponent) entity.findComponent(FiringPatternComponent.class);
-
-            System.out.println("reset pattern");
-
-            firingPatternComponent.setPattern(oldPattern);
-
-        }
+        pattern = new StarPattern(pool, entity, spaceShipComponent.getBulletName());
     }
 }
