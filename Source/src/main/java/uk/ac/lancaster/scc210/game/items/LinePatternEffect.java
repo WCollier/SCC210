c68f1491c564b8b0b86fa0a5776a2ebaecb7a551
buzatu <a.buzatu@lancaster.ac.uk>
2020-02-21 23:33:52 +0000

    Add line pattern effect

--- Source/src/main/java/uk/ac/lancaster/scc210/game/items/LinePatternEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/LinePatternEffect.java
@@ -3,7 +3,6 @@ package uk.ac.lancaster.scc210.game.items;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.engine.pooling.Pool;
 import uk.ac.lancaster.scc210.game.bullets.patterns.Pattern;
-import uk.ac.lancaster.scc210.game.bullets.patterns.StarPattern;
 import uk.ac.lancaster.scc210.game.bullets.patterns.StraightLinePattern;
 import uk.ac.lancaster.scc210.game.ecs.component.FiringPatternComponent;
 import uk.ac.lancaster.scc210.game.ecs.component.SpaceShipComponent;
