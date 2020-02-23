658bc72b13e868fdacaf24e16f42ddfc692c7f06
WCollier <w.collier@lancaster.ac.uk>
2020-02-23 21:43:53 +0000

    Use level manager to create grid.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/states/LevelSelect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/states/LevelSelect.java
@@ -1,10 +1,11 @@
 package uk.ac.lancaster.scc210.game.states;
 
-import org.jsfml.graphics.FloatRect;
-import org.jsfml.graphics.RenderTarget;
-import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.*;
 import org.jsfml.system.Time;
 import org.jsfml.system.Vector2f;
+import org.jsfml.window.Keyboard;
+import org.jsfml.window.event.KeyEvent;
+import uk.ac.lancaster.scc210.engine.InputListener;
 import uk.ac.lancaster.scc210.engine.StateBasedGame;
 import uk.ac.lancaster.scc210.engine.ViewSize;
 import uk.ac.lancaster.scc210.engine.content.FontManager;
@@ -12,17 +13,40 @@ import uk.ac.lancaster.scc210.engine.content.TextureManager;
 import uk.ac.lancaster.scc210.engine.gui.InterfaceGrid;
 import uk.ac.lancaster.scc210.engine.gui.InterfaceList;
 import uk.ac.lancaster.scc210.engine.states.State;
+import uk.ac.lancaster.scc210.game.content.LevelManager;
+import uk.ac.lancaster.scc210.game.level.Level;
 
-public class LevelSelect implements State {
-    private InterfaceGrid interfaceGrid;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LevelSelect implements State, InputListener {
+    private StateBasedGame game;
+
+    private FontManager fontManager;
+
+    private LevelManager levelManager;
 
     private Sprite background;
 
+    private FloatRect viewBounds;
+
+    private InterfaceGrid interfaceGrid;
+
+    private Text exitText;
+
+    private Keyboard.Key pressedKey;
+
     @Override
     public void setup(StateBasedGame game) {
-        FontManager fontManager = (FontManager) game.getServiceProvider().get(FontManager.class);
+        this.game = game;
 
-        ViewSize viewSize = (ViewSize) game.getServiceProvider().get(ViewSize.class);
+        game.addKeyListener(this);
+
+        fontManager = (FontManager) game.getServiceProvider().get(FontManager.class);
+
+        levelManager = (LevelManager) game.getServiceProvider().get(LevelManager.class);
+
+        viewBounds = ((ViewSize) game.getServiceProvider().get(ViewSize.class)).getViewBounds();
 
         TextureManager textureManager = (TextureManager) game.getServiceProvider().get(TextureManager.class);
 
@@ -30,44 +54,61 @@ public class LevelSelect implements State {
 
         background.setScale(2, 2);
 
-        InterfaceList interfaceList = new InterfaceList(game, fontManager.get("font"), Vector2f.ZERO);
+        createGrid();
 
-        interfaceList.addListOption("Level 1", (() -> System.out.println("Hello, world: " + System.currentTimeMillis())));
+        createExitText();
+    }
 
-        interfaceList.addListOption("Level 2", (() -> System.out.println("Thing")));
-
-        interfaceList.addListOption("Level 3", (() -> System.out.println("Item")));
-
-        InterfaceList otherList = new InterfaceList(game, fontManager.get("font"), Vector2f.ZERO);
-
-        otherList.addListOption("Level 4", (() -> System.out.println("Other list")));
-
-        otherList.addListOption("Level 5", (() -> System.out.println("Other list")));
-
-        otherList.addListOption("Level 6", (() -> System.out.println("Other list")));
-
-        InterfaceList anotherList = new InterfaceList(game, fontManager.get("font"), Vector2f.ZERO);
-
-        anotherList.addListOption("Level 7", (() -> System.out.println("Other list")));
-
-        anotherList.addListOption("Level 8", (() -> System.out.println("Other list")));
-
-        anotherList.addListOption("Level 9", (() -> System.out.println("Other list")));
-
-        FloatRect viewBounds = viewSize.getViewBounds();
-
+    private void createGrid() {
+        // TODO: Update this with the develop branch to use the new level manager and level access system
+        // TODO: Prevent the user (or hide) locked levels from being loaded
         interfaceGrid = new InterfaceGrid(game, new Vector2f(viewBounds.width / 4 + 50, viewBounds.height / 3));
 
+        List<Level> levels = new ArrayList<>(levelManager.values());
+
+        InterfaceList interfaceList = null;
+
+        for (int i = 0; i < levels.size(); i++) {
+            if (i % 3 == 0) {
+                System.out.println("I: " + i);
+
+                // i % 3 == 0, will == true when i = 0, so in that instance don't add a new column (as one hasn't been created yet)
+                if (i > 0) {
+                    interfaceGrid.addColumn(interfaceList);
+                }
+
+                interfaceList = new InterfaceList(game, fontManager.get("font"), Vector2f.ZERO);
+            }
+
+            int finalI = i;
+
+            interfaceList.addListOption(String.format("Level %s", i), (() -> System.out.printf("Load level %s here\n", finalI)));
+        }
+
         interfaceGrid.addColumn(interfaceList);
+    }
 
-        interfaceGrid.addColumn(otherList);
+    private void createExitText() {
+        exitText = new Text();
 
-        interfaceGrid.addColumn(anotherList);
+        exitText.setString("Press ESC to go back");
+
+        exitText.setFont(fontManager.get("font"));
+
+        exitText.setPosition(0, 0);
+
+        exitText.setCharacterSize(50);
+
+        exitText.setColor(Color.WHITE);
     }
 
     @Override
     public void update(Time deltaTime) {
         interfaceGrid.update();
+
+        if (game != null && pressedKey != null && pressedKey == Keyboard.Key.ESCAPE) {
+            game.popState();
+        }
     }
 
     @Override
@@ -75,10 +116,12 @@ public class LevelSelect implements State {
         target.draw(background);
 
         target.draw(interfaceGrid);
+
+        target.draw(exitText);
     }
 
     @Override
-    public boolean complete() {
-        return false;
+    public void keyPressed(KeyEvent keyevent) {
+        pressedKey = keyevent.key;
     }
 }
