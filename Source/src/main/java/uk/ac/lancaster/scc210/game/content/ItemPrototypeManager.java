6b6b4450563883fe8d1efb552bab16382c2caca7
WCollier <w.collier@lancaster.ac.uk>
2020-02-23 18:12:24 +0000

    Add alternative item in ItemPrototypeManager.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/content/ItemPrototypeManager.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/content/ItemPrototypeManager.java
@@ -2,13 +2,18 @@ package uk.ac.lancaster.scc210.game.content;
 
 import uk.ac.lancaster.scc210.engine.content.ContentManager;
 import uk.ac.lancaster.scc210.engine.content.TextureManager;
-import uk.ac.lancaster.scc210.game.items.*;
+import uk.ac.lancaster.scc210.game.items.LivesIncreaseEffect;
+import uk.ac.lancaster.scc210.game.items.MissingItemEffect;
+import uk.ac.lancaster.scc210.game.items.SpeedIncreaseEffect;
+import uk.ac.lancaster.scc210.game.items.patterns.LinePatternEffect;
+import uk.ac.lancaster.scc210.game.items.patterns.MorningStarPatternEffect;
+import uk.ac.lancaster.scc210.game.items.patterns.StarPatternEffect;
 import uk.ac.lancaster.scc210.game.pooling.BulletPool;
 import uk.ac.lancaster.scc210.game.prototypes.ItemPrototype;
 
 public class ItemPrototypeManager extends ContentManager<ItemPrototype> {
     public ItemPrototypeManager(TextureManager textureManager, BulletPool bulletPool) {
-        super(null);
+        super(new ItemPrototype(textureManager.get(""), new MissingItemEffect()));
 
         put("test-item", new ItemPrototype(textureManager.get("spritesheet.png:item"), new SpeedIncreaseEffect()));
 
