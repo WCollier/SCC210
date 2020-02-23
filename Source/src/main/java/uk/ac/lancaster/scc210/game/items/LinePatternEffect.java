2c026a2f533a09a9b22ce3d04ab95ae4818fbde7
WCollier <w.collier@lancaster.ac.uk>
2020-02-23 16:28:50 +0000

    Firing Pattern Items now inherit from the FiringPatternEffect class.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/items/LinePatternEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/LinePatternEffect.java
@@ -2,50 +2,18 @@ package uk.ac.lancaster.scc210.game.items;
 
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.engine.pooling.Pool;
-import uk.ac.lancaster.scc210.game.bullets.patterns.Pattern;
 import uk.ac.lancaster.scc210.game.bullets.patterns.StraightLinePattern;
-import uk.ac.lancaster.scc210.game.ecs.component.FiringPatternComponent;
 import uk.ac.lancaster.scc210.game.ecs.component.SpaceShipComponent;
 
-public class LinePatternEffect implements ItemEffect {
-    private Pattern oldPatern;
-
-    private Pool pool;
-
-    public LinePatternEffect (Pool pool)
-    {
-        this.pool = pool;
-    }
-    @Override
-    public void react(Entity entity) {
-
-        if (entity.hasComponent(FiringPatternComponent.class ) && entity.hasComponent(SpaceShipComponent.class)){
-
-
-            SpaceShipComponent spaceShipComponent = (SpaceShipComponent) entity.findComponent(SpaceShipComponent.class);
-
-            FiringPatternComponent firingPatternComponent = (FiringPatternComponent) entity.findComponent(FiringPatternComponent.class);
-
-            if(oldPatern == null) {
-
-                oldPatern = firingPatternComponent.getPattern();
-            }
-
-            firingPatternComponent.setPattern(new StraightLinePattern(pool, entity, spaceShipComponent.getBulletName()));
-
-        }
-
+public class LinePatternEffect extends FiringPatternEffect {
+    public LinePatternEffect(Pool pool) {
+        super(pool);
     }
 
     @Override
-    public void reset(Entity entity) {
-        if (entity.hasComponent(FiringPatternComponent.class) && entity.hasComponent(SpaceShipComponent.class)) {
+    void setPattern(Entity entity) {
+        SpaceShipComponent spaceShipComponent = (SpaceShipComponent) entity.findComponent(SpaceShipComponent.class);
 
-
-            FiringPatternComponent firingPatternComponent = (FiringPatternComponent) entity.findComponent(FiringPatternComponent.class);
-
-            firingPatternComponent.setPattern(oldPatern);
-
-        }
+        pattern = new StraightLinePattern(pool, entity, spaceShipComponent.getBulletName());
     }
 }
--- Source/src/main/java/uk/ac/lancaster/scc210/game/items/MorningStarPatternEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/MorningStarPatternEffect.java
@@ -3,50 +3,17 @@ package uk.ac.lancaster.scc210.game.items;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.engine.pooling.Pool;
 import uk.ac.lancaster.scc210.game.bullets.patterns.MorningStarPattern;
-import uk.ac.lancaster.scc210.game.bullets.patterns.Pattern;
-import uk.ac.lancaster.scc210.game.ecs.component.FiringPatternComponent;
 import uk.ac.lancaster.scc210.game.ecs.component.SpaceShipComponent;
 
-public class MorningStarPatternEffect implements ItemEffect {
-    private Pattern oldPattern;
-
-    private Pool pool;
-
+public class MorningStarPatternEffect extends FiringPatternEffect {
     public MorningStarPatternEffect(Pool pool) {
-        this.pool = pool;
+        super(pool);
     }
 
     @Override
-    public void react(Entity entity) {
+    void setPattern(Entity entity) {
+        SpaceShipComponent spaceShipComponent = (SpaceShipComponent) entity.findComponent(SpaceShipComponent.class);
 
-
-        if (entity.hasComponent(FiringPatternComponent.class) && entity.hasComponent(SpaceShipComponent.class)) {
-
-
-            SpaceShipComponent spaceShipComponent = (SpaceShipComponent) entity.findComponent(SpaceShipComponent.class);
-
-            FiringPatternComponent firingPatternComponent = (FiringPatternComponent) entity.findComponent(FiringPatternComponent.class);
-
-            if (oldPattern == null) {
-                oldPattern = firingPatternComponent.getPattern();
-            }
-
-            firingPatternComponent.setPattern(new MorningStarPattern(pool, entity, spaceShipComponent.getBulletName()));
-
-        }
-    }
-
-    @Override
-    public void reset(Entity entity) {
-        if (entity.hasComponent(FiringPatternComponent.class) && entity.hasComponent(SpaceShipComponent.class)) {
-
-
-            FiringPatternComponent firingPatternComponent = (FiringPatternComponent) entity.findComponent(FiringPatternComponent.class);
-
-            System.out.println("reset pattern");
-
-            firingPatternComponent.setPattern(oldPattern);
-
-        }
+        pattern = new MorningStarPattern(pool, entity, spaceShipComponent.getBulletName());
     }
 }
