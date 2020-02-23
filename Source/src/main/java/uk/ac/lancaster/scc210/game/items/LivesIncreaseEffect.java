8858bdb702d8ad03c320720b3fd90af59c8a274e
WCollier <w.collier@lancaster.ac.uk>
2020-02-23 18:13:37 +0000

    Update items to use new abstractions

--- Source/src/main/java/uk/ac/lancaster/scc210/game/items/LivesIncreaseEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/LivesIncreaseEffect.java
@@ -3,10 +3,11 @@ package uk.ac.lancaster.scc210.game.items;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.game.ecs.component.LivesComponent;
 
-public class LivesIncreaseEffect implements ItemEffect {
-
+public class LivesIncreaseEffect extends PowerUpEffect {
     @Override
     public void react(Entity entity) {
+        super.react(entity);
+
         if (entity.hasComponent(LivesComponent.class)) {
             LivesComponent livesComponent = (LivesComponent) entity.findComponent(LivesComponent.class);
 
@@ -14,10 +15,5 @@ public class LivesIncreaseEffect implements ItemEffect {
 
             livesComponent.setLives(newLives);
         }
-    }
-
-    @Override
-    public void reset(Entity entity) {
-        //TODO - does anything need to go here? Lives reset at death anyway
     }
 }
--- Source/src/main/java/uk/ac/lancaster/scc210/game/items/SpeedIncreaseEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/SpeedIncreaseEffect.java
@@ -1,17 +1,29 @@
 package uk.ac.lancaster.scc210.game.items;
 
+import org.jsfml.system.Time;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.game.ecs.component.SpeedComponent;
 
-public class SpeedIncreaseEffect implements ItemEffect {
+public class SpeedIncreaseEffect extends TimedItemEffect {
     private int oldSpeed;
+
+    public SpeedIncreaseEffect() {
+        super(Time.getSeconds(5));
+
+        // Entities shouldn't have a speed of -1
+        oldSpeed = -1;
+    }
 
     @Override
     public void react(Entity entity) {
+        super.react(entity);
+
         if (entity.hasComponent(SpeedComponent.class)) {
             SpeedComponent speedComponent = (SpeedComponent) entity.findComponent(SpeedComponent.class);
 
-            oldSpeed = speedComponent.getStartingSpeed();
+            if (oldSpeed == -1) {
+                oldSpeed = speedComponent.getSpeed();
+            }
 
             speedComponent.setSpeed(20);
         }
@@ -22,7 +34,7 @@ public class SpeedIncreaseEffect implements ItemEffect {
         if (entity.hasComponent(SpeedComponent.class)) {
             SpeedComponent speedComponent = (SpeedComponent) entity.findComponent(SpeedComponent.class);
 
-            speedComponent.setSpeed(speedComponent.getStartingSpeed());
+            speedComponent.setSpeed(oldSpeed);
         }
     }
 }
