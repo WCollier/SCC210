b80fd34b4c5631bbfe4cdb2fe0e4bf18014fefc7
WCollier <w.collier@lancaster.ac.uk>
2020-02-24 18:28:54 +0000

    Remove ItemEffects from BulletComponent to PlayerComponent

--- Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/component/BulletComponent.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/component/BulletComponent.java
@@ -2,42 +2,15 @@ package uk.ac.lancaster.scc210.game.ecs.component;
 
 import uk.ac.lancaster.scc210.engine.ecs.Component;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
-import uk.ac.lancaster.scc210.game.bullets.effects.BulletEffect;
-import uk.ac.lancaster.scc210.game.bullets.effects.FreezeBulletEffect;
-import uk.ac.lancaster.scc210.game.bullets.effects.LivesEffect;
 
 public class BulletComponent implements Component {
     private final Entity creator;
 
-    private final BulletEffect defaultEffect;
-
-    private BulletEffect bulletEffect;
-
     public BulletComponent(Entity creator) {
         this.creator = creator;
-
-        //bulletEffect = new LivesEffect();
-
-        bulletEffect = new FreezeBulletEffect();
-
-        defaultEffect = new LivesEffect();
     }
 
     public Entity getCreator() {
         return creator;
-    }
-
-    public BulletEffect getBulletEffect() {
-        return bulletEffect;
-    }
-
-    public void setBulletEffectToDefault() {
-        this.bulletEffect = defaultEffect;
-
-        defaultEffect.setDead(false);
-
-        if (bulletEffect == defaultEffect) {
-            System.out.println("Set to default");
-        }
     }
 }
--- Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/component/PlayerComponent.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/component/PlayerComponent.java
@@ -2,30 +2,43 @@ package uk.ac.lancaster.scc210.game.ecs.component;
 
 import org.jsfml.system.Vector2f;
 import uk.ac.lancaster.scc210.engine.ecs.Component;
+import uk.ac.lancaster.scc210.game.bullets.effects.BulletEffect;
+import uk.ac.lancaster.scc210.game.bullets.effects.FreezeBulletEffect;
+import uk.ac.lancaster.scc210.game.bullets.effects.LivesEffect;
 import uk.ac.lancaster.scc210.game.items.ItemEffect;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class PlayerComponent implements Component {
-    private Set<ItemEffect> currentEffects;
+    private final BulletEffect defaultBulletEffect;
+
+    private Set<ItemEffect> currentItemEffects;
+
+    private BulletEffect bulletEffect;
 
     private Vector2f spawnPoint;
 
     private int currentScore;
 
     public PlayerComponent() {
-        currentEffects = new HashSet<>();
+        defaultBulletEffect = new LivesEffect();
+
+        bulletEffect = new FreezeBulletEffect();
+
+        currentItemEffects = new HashSet<>();
+
+        bulletEffect = new FreezeBulletEffect();
 
         currentScore = 0;
     }
 
-    public Set<ItemEffect> getCurrentEffects() {
-        return currentEffects;
+    public Set<ItemEffect> getCurrentItemEffects() {
+        return currentItemEffects;
     }
 
-    public void setCurrentEffects(Set<ItemEffect> currentEffects) {
-        this.currentEffects.addAll(currentEffects);
+    public void setCurrentItemEffects(Set<ItemEffect> currentItemEffects) {
+        this.currentItemEffects.addAll(currentItemEffects);
     }
 
     public void changeScore(int score) {
@@ -46,5 +59,19 @@ public class PlayerComponent implements Component {
 
     public Vector2f getSpawnPoint() {
         return spawnPoint;
+    }
+
+    public BulletEffect getBulletEffect() {
+        return bulletEffect;
+    }
+
+    public void setBulletEffectToDefault() {
+        this.bulletEffect = defaultBulletEffect;
+
+        defaultBulletEffect.setDead(false);
+
+        if (bulletEffect == defaultBulletEffect) {
+            System.out.println("Set to default");
+        }
     }
 }
