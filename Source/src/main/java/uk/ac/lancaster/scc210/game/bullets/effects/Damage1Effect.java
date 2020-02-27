4cd58abbae22c7458a53c3ed4e1536dd13b59688
buzatu <a.buzatu@lancaster.ac.uk>
2020-02-27 15:52:28 +0000

    Add Damage Effect 1&2

---
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/bullets/effects/Damage1Effect.java
@@ -0,0 +1,9 @@
+package uk.ac.lancaster.scc210.game.bullets.effects;
+
+public class Damage1Effect extends DamageEffect {
+    public Damage1Effect() {
+        super(1);
+    }
+
+
+}
--- Source/src/main/java/uk/ac/lancaster/scc210/game/bullets/effects/DegradingLivesEffect.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/bullets/effects/DegradingLivesEffect.java
@@ -2,7 +2,7 @@ package uk.ac.lancaster.scc210.game.bullets.effects;
 
 import org.jsfml.system.Time;
 
-public class DegradingLivesEffect extends LivesEffect {
+public class DegradingLivesEffect extends DamageEffect {
     public DegradingLivesEffect(Time degradeTime) {
         super((int) degradeTime.asSeconds());
     }
