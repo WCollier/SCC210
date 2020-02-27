8e2f2bc46086c633089fd469d7575d7de1966f5a
buzatu <a.buzatu@lancaster.ac.uk>
2020-02-27 15:55:17 +0000

    Add Health Effect Item 1&2

---
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/Health1EffectItem.java
@@ -0,0 +1,8 @@
+package uk.ac.lancaster.scc210.game.items;
+
+public class Health1EffectItem extends HealthEffectItem {
+
+    public Health1EffectItem() {
+        super(1);
+    }
+}
---
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/items/Health2EffectItem.java
@@ -0,0 +1,7 @@
+package uk.ac.lancaster.scc210.game.items;
+
+public class Health2EffectItem extends HealthEffectItem {
+    public Health2EffectItem() {
+        super(2);
+    }
+}
