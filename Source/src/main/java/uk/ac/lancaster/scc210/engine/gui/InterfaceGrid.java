9bd2298a351cd967750208b608d4a7cb48346ed5
WCollier <w.collier@lancaster.ac.uk>
2020-02-29 17:56:06 +0000

    Fix column padding.

--- Source/src/main/java/uk/ac/lancaster/scc210/engine/gui/InterfaceGrid.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/engine/gui/InterfaceGrid.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 public class InterfaceGrid implements InputListener, Drawable {
     private final int FIRST_LIST_PADDING = 50;
 
-    private final int LIST_WIDTH_PADDING = 130;
+    private final int LIST_WIDTH_PADDING = 280;
 
     private final ArrayList<InterfaceList> lists;
 
