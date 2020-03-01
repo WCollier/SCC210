bf4b2b2acc177c9dfd6b5b142d539ba26b50b1a6
WCollier <w.collier@lancaster.ac.uk>
2020-03-01 20:49:15 +0000

    Add counter to SoundManager to prevent going over the max sounds allowed by SFML.

--- Source/src/main/java/uk/ac/lancaster/scc210/engine/content/SoundManager.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/engine/content/SoundManager.java
@@ -4,6 +4,10 @@ import org.jsfml.audio.Sound;
 import org.jsfml.audio.SoundSource;
 
 public class SoundManager extends ContentManager<Sound> {
+    private final int MAX_SOUNDS = 255;
+
+    private int currentSounds;
+
     /**
      * Instantiates a new Content manager.
      *
@@ -12,15 +16,33 @@ public class SoundManager extends ContentManager<Sound> {
         super(new Sound());
 
         soundBufferManager.content.forEach((key, value) -> put(key, new Sound(value)));
+
+        currentSounds = 0;
+    }
+
+    @Override
+    public Sound get(String key) {
+        Sound sound = super.get(key);
+
+        if (sound.getStatus() != SoundSource.Status.STOPPED) {
+            currentSounds++;
+
+            return sound;
+        }
+
+        currentSounds--;
+
+        return null;
     }
 
     public void playSound(String name) {
-        if (content.containsKey(name)) {
+        if (content.containsKey(name) && currentSounds < MAX_SOUNDS) {
             Sound sound = content.get(name);
 
             // Only play the sound if the sound has been flagged as stop. This is done to prevent the sound from restarting while already playing
             if (sound.getStatus() == SoundSource.Status.STOPPED) {
                 sound.play();
+
             }
         }
     }
