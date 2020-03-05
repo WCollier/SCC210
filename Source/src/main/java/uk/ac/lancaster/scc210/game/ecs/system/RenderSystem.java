afe806d596b7ff5f86202c4324e9b01935e4e8d6
WCollier <w.collier@lancaster.ac.uk>
2020-03-05 16:47:50 +0000

    Add basic vertex arrays for stars.

--- Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/system/RenderSystem.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/game/ecs/system/RenderSystem.java
@@ -1,20 +1,35 @@
 package uk.ac.lancaster.scc210.game.ecs.system;
 
-import org.jsfml.graphics.Drawable;
-import org.jsfml.graphics.RenderStates;
-import org.jsfml.graphics.RenderTarget;
+import org.jsfml.graphics.*;
 import org.jsfml.system.Time;
+import org.jsfml.system.Vector2f;
+import uk.ac.lancaster.scc210.engine.ViewSize;
 import uk.ac.lancaster.scc210.engine.ecs.Entity;
 import uk.ac.lancaster.scc210.engine.ecs.World;
 import uk.ac.lancaster.scc210.engine.ecs.system.IterativeSystem;
 import uk.ac.lancaster.scc210.game.ecs.component.*;
+import uk.ac.lancaster.scc210.game.states.Playing;
 
 import java.util.Collection;
+import java.util.Random;
 
 /**
  * System used to draw non-animated Sprites to the Screen
  */
 public class RenderSystem extends IterativeSystem {
+    private final Color[] STAR_COLOURS = {
+            new Color(153, 204, 255),
+            new Color(255, 255, 255),
+            new Color(255, 102, 0),
+            new Color(255, 0, 0)
+    };
+
+    private final VertexArray stars;
+
+    private final Random random;
+
+    private final ViewSize viewSize;
+
     /**
      * Instantiates a new Iterative system.
      *
@@ -23,7 +38,19 @@ public class RenderSystem extends IterativeSystem {
     public RenderSystem(World world) {
         super(world);
 
+        stars = new VertexArray(PrimitiveType.POINTS);
+
+        random = new Random();
+
+        viewSize = (ViewSize) world.getServiceProvider().get(ViewSize.class);
+
         entities = world.getEntitiesWithAny(SpriteComponent.class, AsteroidComponent.class);
+
+        for (int i = 0; i < 50; i++) {
+            Vertex star = new Vertex(randPosition(), STAR_COLOURS[randInt(0, STAR_COLOURS.length - 1)]);
+
+            stars.add(star);
+        }
     }
 
     @Override
@@ -54,6 +81,8 @@ public class RenderSystem extends IterativeSystem {
 
     @Override
     public void draw(RenderTarget target) {
+        target.draw(stars);
+
         for (Entity entity : entities) {
             SpriteComponent spriteComponent = (SpriteComponent) entity.findComponent(SpriteComponent.class);
 
@@ -79,7 +108,7 @@ public class RenderSystem extends IterativeSystem {
                     if (livesComponent.getLives() == 1) {
                         flashComponent.setShaderRed();
                     }
-                    
+
                 } else {
                     // If the previous shader was white, reset back to white
                     flashComponent.setShaderWhite();
@@ -90,5 +119,15 @@ public class RenderSystem extends IterativeSystem {
 
             target.draw(drawable, renderStates);
         }
+    }
+
+    private Vector2f randPosition() {
+        FloatRect viewBounds = viewSize.getViewBounds();
+
+        return new Vector2f(randInt(0, (int) viewBounds.width), randInt(Playing.INFO_BOX_HEIGHT, (int) viewBounds.height));
+    }
+
+    private int randInt(int min, int max) {
+        return random.nextInt((max - min) + 1) + min;
     }
 }
