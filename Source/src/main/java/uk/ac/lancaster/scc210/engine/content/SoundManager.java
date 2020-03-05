42d094175f111b00c9f9182a4b5ed23e05bfcaf1
WCollier <w.collier@lancaster.ac.uk>
2020-03-05 15:15:14 +0000

    Sounds should stop if they are requested but only if they've been playing for more than 3 seconds.

--- Source/src/main/java/uk/ac/lancaster/scc210/engine/content/SoundManager.java
+++ Source/src/main/java/uk/ac/lancaster/scc210/engine/content/SoundManager.java
@@ -1,12 +1,16 @@
 package uk.ac.lancaster.scc210.engine.content;
 
+import jdk.net.SocketFlow;
 import org.jsfml.audio.Sound;
 import org.jsfml.audio.SoundSource;
+import org.jsfml.system.Time;
 
 /**
  * The type Sound manager.
  */
 public class SoundManager extends ContentManager<Sound> {
+    private final Time TIME_RESTART = Time.getSeconds(3);
+
     private final int MAX_SOUNDS = 255;
 
     private int currentSounds;
@@ -48,10 +52,16 @@ public class SoundManager extends ContentManager<Sound> {
         if (content.containsKey(name) && currentSounds < MAX_SOUNDS) {
             Sound sound = content.get(name);
 
+            // Cut the sound if it is playing for more than TIME_RESTART allows
+            if (sound.getStatus() == SoundSource.Status.PLAYING && sound.getPlayingOffset().asSeconds() > TIME_RESTART.asSeconds()) {
+                sound.stop();
+
+                sound.play();
+            }
+
             // Only play the sound if the sound has been flagged as stop. This is done to prevent the sound from restarting while already playing
             if (sound.getStatus() == SoundSource.Status.STOPPED) {
                 sound.play();
-
             }
         }
     }
